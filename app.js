0ee785b5fd4333eaab4a9ec40b77ebf7c562372f
marirhs <18211a04l2@bvrit.ac.in>
2022-05-12 18:33:35 +0530

    Added Aggregation and Pagination

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+const connectionString = process.env.MONGODB_CS || 'mongodb://localhost:27017/userdb';
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+mongoose.connect(connectionString, { useNewUrlParser: true })
+.then(() => {
+  console.log('Database connection successful')
+})
+.catch(err => {
+  console.error('Database connection error')
+});
 
 let UserModel = require('./user')
 
@@ -24,15 +31,17 @@ let UserModel = require('./user')
 //    .catch(err => {
 //      console.error(err)
 //    })
-
+app.use(express.json());
 //api routes for creating and updating data
 app.listen(port, () => {
 	console.log(`Server has started! Running at http://localhost:${port}`);
 })
 
 //create a new user
-app.post('/api/users', (req, res) => {
+app.post('/api/users',(req, res) => {
+    console.log('Request body:', req.body)
     let user = new UserModel(req.body);
+    console.log(user)
     user.save()
         .then(doc => {
             res.send(doc);
@@ -48,6 +57,67 @@ app.post('/api/users', (req, res) => {
 //get all users
 app.get('/api/users', (req, res) => {
     UserModel.find()
+        .then(doc => {
+            res.send(doc);
+        }
+        )
+        .catch(err => {
+            res.status(500).send(err);
+        }
+        )
+}
+)
+
+// aggregation pipeline
+app.get('/api/users/aggregate', (req, res) => {
+    UserModel.aggregate([
+        {
+            $match: {
+                "Email": {
+                    $regex: "gmail"
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$Email",
+                count: {
+                    $sum: 1
+                }
+            }
+        }
+    ])
+        .then(doc => {
+            res.send(doc);
+        }
+        )
+        .catch(err => {
+            res.status(500).send(err);
+        }
+        )
+}
+)
+//aggregation pipeline with pagination
+app.get('/api/users/aggregate/:page/:limit', (req, res) => {
+    UserModel.aggregate([
+        {
+            $match: {
+                "Email": {
+                    $regex: "gmail"
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$Email",
+                count: {
+                    $sum: 1
+                }
+            }
+        }
+    ])
+        .skip(req.params.page * req.params.limit)
+        .limit(req.params.limit)
         .then(doc => {
             res.send(doc);
         }
@@ -90,6 +160,21 @@ app.put('/api/users/:id', (req, res) => {
 //delete a user by id
 app.delete('/api/users/:id', (req, res) => {
     UserModel.findByIdAndDelete(req.params.id)
+        .then(doc => {
+            res.send(doc);
+        }
+        )
+        .catch(err => {
+            res.status(500).send(err);
+        }
+        )
+}
+)
+//get users with pagination
+app.get('/api/users/:page/:limit', (req, res) => {
+    UserModel.find()
+        .skip(req.params.page * req.params.limit)
+        .limit(req.params.limit)
         .then(doc => {
             res.send(doc);
         }
